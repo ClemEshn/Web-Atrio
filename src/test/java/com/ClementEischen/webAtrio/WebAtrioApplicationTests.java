8a38856287b1a6985070182757e02121963f9b84
ClemEshn <contact.klems@gmail.com>
2025-04-29 13:15:50 +0200

    project init

---
+++ src/test/java/com/ClementEischen/webAtrio/WebAtrioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ClementEischen.webAtrio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAtrioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
